1ca0636bbbb0e5dbd95753c78d4bf5a320a84015
nichol6s <nicholas.jake15@gmail.com>
2024-04-18 21:47:40 -0300

    add: Adicionar as telas de navegação Tabs e Linear Gradient

---
+++ src/app/(tabs)/profile.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function Profile(){
+    return(
+        <View className="flex w-full mt-40">
+            <Text className="text-purple-600">Profile Page</Text>
+        </View>
+    )
+}
