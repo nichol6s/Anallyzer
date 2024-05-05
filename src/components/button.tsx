40b87849dee488c910e2e67e45cb301c8e95d90b
nichol6s <nicholas.jake15@gmail.com>
2024-05-05 19:35:04 -0300

    add: Adicionar componente Button e Imagem na pagina Welcome

---
+++ src/components/button.tsx
@@ -0,0 +1,16 @@
+import { Text, View, TouchableOpacityProps, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+type Props = TouchableOpacityProps & {
+    title: string
+}
+
+export default function Button({ title, ...rest }: Props) {
+    return (
+        <TouchableOpacity activeOpacity={0.7} {...rest}>
+            <View className='w-[344] h-[50] bg-[#BFB7FD] items-center justify-center rounded-full' >
+                <Text className='text-black text-base'>{title}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
