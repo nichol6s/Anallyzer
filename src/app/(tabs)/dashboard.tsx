2b1763ca54906996336994383ab8ca56b03b4076
nichol6s <nicholas.jake15@gmail.com>
2024-05-08 18:32:21 -0300

    chore: Update dashboard and profile pages with gradient backgrounds

--- src/app/(tabs)/dashboard.tsx
+++ src/app/(tabs)/dashboard.tsx
@@ -1,10 +1,21 @@
-import { Text, View } from 'react-native'
 import React from 'react'
+import { Text, View } from 'react-native'
+
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function Dashboard() {
   return (
-    <View className='flex w-full mt-40'>
-      <Text className='text-green-700'>Dashboard Page</Text>
-    </View>
+    <LinearGradient
+      colors={['#DAD5FB', '#FFF']}
+      start={[0, 0]}
+      end={[0, 1]}
+      className="flex-1 p-6"
+    >
+
+      <View className='flex w-full mt-40'>
+        <Text className='text-green-700'>Dashboard Page</Text>
+      </View>
+
+    </LinearGradient>
   )
 }
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,9 +1,19 @@
+import React from "react"
 import { View, Text } from "react-native"
 
-export default function Profile(){
-    return(
-        <View className="flex w-full mt-40">
-            <Text className="text-purple-600">Profile Page</Text>
-        </View>
+import { LinearGradient } from "expo-linear-gradient"
+
+export default function Profile() {
+    return (
+        <LinearGradient
+            colors={['#DAD5FB', '#FFF']}
+            start={[0, 0]}
+            end={[0, 1]}
+            className="flex-1 p-6"
+        >
+            <View className="flex w-full mt-40">
+                <Text className="text-purple-600">Profile Page</Text>
+            </View>
+        </LinearGradient>
     )
 }
