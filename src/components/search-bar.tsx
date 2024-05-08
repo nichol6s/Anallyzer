db430bc776b589e2022f9f9bbccf1e39c8bdfbf0
VitorKubica <vitor05kubica12@gmail.com>
2024-05-08 12:12:25 -0300

    feat: Add login button functionality to redirect to overview page

---
+++ src/components/search-bar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { TextInput, TextInputProps, View } from 'react-native'
+import { colors } from '../styles/colors'
+
+function SearchBar({ children }: { children: React.ReactNode }) {
+    return (
+        <View className="w-full h-[55px] flex-row items-center gap-3 pl-8 border border-gray-100 rounded-full">
+            {children}
+        </View>
+    )
+}
+
+function Field({ ...rest }: TextInputProps) {
+    return (
+        <TextInput
+            className="flex-1 text-black-100 text-md font-light"
+            placeholderTextColor={colors.black[100]}
+            {...rest}
+        />
+        
+    )
+}
+
+SearchBar.Field = Field
+
+export { SearchBar }
+
