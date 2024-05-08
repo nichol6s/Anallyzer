b95026ce58b3ba48282af3c76e3955b74b25860e
VitorKubica <vitor05kubica12@gmail.com>
2024-05-08 10:39:51 -0300

    feat: add login page, without inputs

--- src/app/(stack)/login.tsx
+++ src/app/(stack)/login.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Text, View } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import { useRouter } from 'expo-router'
+import React from 'react'
+import { Pressable, Text, View } from 'react-native'
 
 import Arrow from '@/src/components/arrow-back'
+import Button from '@/src/components/button'
 export default function Login() {
     const router = useRouter()
 
@@ -17,8 +18,15 @@ export default function Login() {
             <View className='items-left mt-16'>
                 <Arrow onPress={() => router.back()} />
             </View>
-            <View className=''>
-                <Text>login</Text>
+            <View className='my-[30px]'>
+                <Text className='text-4xl font-medium tracking-tight leading-[44px]'>Login</Text>
+            </View>
+            <Button title='Login' />
+            <View className='flex-row justify-center mt-4'>
+                <Text className='font-regular text-gray-200'>Ainda não possui uma conta? </Text>
+                <Pressable onPress={() => router.push("/login")}>
+                    <Text className='font-bold underline text-gray-200'>Registrar-se</Text>
+                </Pressable>
             </View>
         </LinearGradient>
     )
