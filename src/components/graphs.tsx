359167328fa3a4056c6437b796d7e8530848f3cf
nichol6s <nicholas.jake15@gmail.com>
2024-05-13 22:17:13 -0300

    chore: Update BasicGraph component to include title, percentage, quantity, and valueLastYear props

--- src/components/graphs.tsx
+++ src/components/graphs.tsx
@@ -1,11 +1,34 @@
 import React from "react"
-import { View } from "react-native"
+import { View, Text } from "react-native"
 
+import { AntDesign } from "@expo/vector-icons"
+import { colors } from "@/styles/colors"
 
-function BasicGraph() {
+type BasicGraphProps = {
+    title: string
+    percentage?: string
+    quantity: number
+    valueLastYear: number
+}
+
+function BasicGraph({ title, percentage, quantity, valueLastYear }: BasicGraphProps) {
     return (
         <View className=" w-44 h-36 p-4 rounded-3xl bg-white">
 
+            <View className="flex-row gap-2">
+                <Text className="font-semibold text-sm">{title}</Text>
+
+                {percentage && (
+                    <Text className={`font-semibold text-sm ${percentage.includes('+') ? 'text-green' : 'text-red'}`}>
+                        {percentage}
+                        <AntDesign name={percentage.includes('+') ? "arrowup" : "arrowdown"} size={12} color={percentage.includes('+') ? colors.green : colors.red} />
+                    </Text>
+                )}
+            </View>
+
+            <Text className="font-semibold text-2xl mt-2">{quantity}</Text>
+
+            <Text className="font-regular text-gray-200 text-sm mt-2">Comparado com {valueLastYear} ano passado</Text>
         </View>
     )
 }
