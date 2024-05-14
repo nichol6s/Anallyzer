fe7f5d9b54788edf0288eb22dc156e84c7d0db81
nichol6s <nicholas.jake15@gmail.com>
2024-05-14 17:13:26 -0300

    chore: Update DetailedInsights component with ScrollView and additional graphs

--- src/app/(stack)/detailed-insights.tsx
+++ src/app/(stack)/detailed-insights.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 
 import { useRouter } from 'expo-router'
 import { LinearGradient } from 'expo-linear-gradient'
-import Arrow from '@/components/arrow-back'
 
+import Arrow from '@/components/arrow-back'
+import { Graph } from '@/components/graphs'
 
 export default function DetailedInsights() {
     const router = useRouter()
@@ -16,13 +17,74 @@ export default function DetailedInsights() {
             end={[0, 1]}
             className="flex-1 p-6"
         >
-            <View className='items-left mt-12 mb-6'>
-                <Arrow onPress={() => router.back()} />
-            </View>
+            <ScrollView className='px-2' showsVerticalScrollIndicator={false}>
+                <View className='items-left mt-12 mb-6'>
+                    <Arrow onPress={() => router.back()} />
+                </View>
 
-            <View className='items-left'>
-                <Text className='text-black text-2xl font-mediumj'>Novidades da Semana: Camp...</Text>
-            </View>
+
+                <View className='items-left'>
+                    <Text className='text-2xl font-mediumj'>Novidades da Semana: Camp...</Text>
+                </View>
+
+                <View className='mt-6 gap-4'>
+                    <Graph.LargeGraph
+                        quantity={33.889}
+                        title="Clicks"
+                        percentage="+2,5%"
+                    />
+
+                    <Text className='text-sm font-light text-gray-200'>
+                        Parabéns pelo aumento de 4,5% nos cliques em relação ao esperado! Isso indica que sua campanha conseguiu atrair a atenção dos destinatários e incentivá-los a interagir com seu conteúdo.
+                    </Text>
+                </View>
+
+                <View className='mt-6 gap-4'>
+                    <Graph.LargeGraph
+                        quantity={56.644}
+                        title="Aberturas"
+                        percentage="+2%"
+                    />
+
+                    <Text className='text-sm font-light text-gray-200'>
+                        Esse aumento de 2% mostra que sua linha de assunto e pré-visualização estão chamando a atenção do público-alvo. Sua campanha está despertanto interesse, parabéns!
+                    </Text>
+                </View>
+
+                <View className='mt-6 gap-4'>
+                    <Graph.LargeGraph
+                        quantity={86.644}
+                        title="Envios"
+                        percentage="+0,5%"
+                    />
+
+                    <Text className='text-sm font-light text-gray-200'>
+                        Embora o crescimento de 0,5% nos envios seja modesto, é encorajador ver que sua campanha está alcançando um número maior de destinatários do que o esperado.
+                    </Text>
+                </View>
+
+                <View className='mt-6 gap-4'>
+                    <Graph.LargeGraph
+                        quantity={1.678}
+                        title="Leads"
+                        percentage="-38%"
+                    />
+
+                    <Text className='text-sm font-light text-gray-200'>
+                        Apesar da diminuição de 38% no número de leads, é importante analisar os motivos por trás disso. Revise sua oferta ou conteúdo para garantir que esteja alinhado com as expectativas e necessidades do seu público-alvo.
+                    </Text>
+                </View>
+
+                <View className='mt-6 gap-4'>
+                    <Graph.CustomerGrowth />
+
+                    <Text className='text-sm font-light text-gray-200'>
+                        O gráfico mostra um crescimento consistente ao longo dos 6 meses da campanha, com um aumento notável no número de  clientes Homens. Isso é encorajador, pois indica que sua campanha está conseguindo atrair e reter eles.
+                    </Text>
+                </View>
+            </ScrollView>
+
+
         </LinearGradient>
     )
 }
