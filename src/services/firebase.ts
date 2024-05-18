f19e0f5c7db7814e5394029352cec3c1e7cd21c9
nichol6s <nicholas.jake15@gmail.com>
2024-05-18 16:00:41 -0300

    feat: Add firebase dependency and initialize Firebase app

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6_G7VrKOYJnfNSiMguM56-krY6JDHaZU",
+  authDomain: "anallyzer-backend.firebaseapp.com",
+  projectId: "anallyzer-backend",
+  storageBucket: "anallyzer-backend.appspot.com",
+  messagingSenderId: "542399931138",
+  appId: "1:542399931138:web:db4a6012e8e3a5dbaf581b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
