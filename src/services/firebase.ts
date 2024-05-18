a775c7a18a647152f9c4a5906d5fd4df627ecf43
nichol6s <nicholas.jake15@gmail.com>
2024-05-18 20:02:59 -0300

    feat: Add @react-native-async-storage/async-storage dependency and initialize Firebase app, login page is functional but signup not

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore, doc, setDoc, addDoc, collection } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD6_G7VrKOYJnfNSiMguM56-krY6JDHaZU",
   authDomain: "anallyzer-backend.firebaseapp.com",
@@ -15,3 +13,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+export { app, auth, db, doc, setDoc, addDoc, collection } 
