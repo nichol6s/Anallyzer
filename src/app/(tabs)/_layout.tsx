98f17c10a324d64c7c84b5159f216b645ae398ae
nichol6s <nicholas.jake15@gmail.com>
2024-06-11 14:09:17 -0300

    chore: Update tabBar colors in TabsLayout component

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -6,37 +6,29 @@ import React from "react"
 
 export default function TabsLayout() {
     return (
-        <Tabs screenOptions={{ headerShown: false, tabBarLabel: () => false }} >
+        <Tabs screenOptions={{ headerShown: false, tabBarLabel: () => false, tabBarActiveTintColor: "black", tabBarInactiveTintColor: "gray" }} >
             <Tabs.Screen
                 name="overview"
                 options={{
                     tabBarIcon: ({ color }) => <Octicons name="home" size={22} color={color} />,
-                    tabBarInactiveTintColor: "gray",
-                    tabBarActiveTintColor: "black",
                 }}
             />
             <Tabs.Screen
                 name="dashboard"
                 options={{
                     tabBarIcon: ({ color }) => <Feather name="pie-chart" size={22} color={color} />,
-                    tabBarInactiveTintColor: "gray",
-                    tabBarActiveTintColor: "black",
                 }}
             />
             <Tabs.Screen
                 name="insights"
                 options={{
                     tabBarIcon: ({ color }) => <Entypo name="light-bulb" size={22} color={color} />,
-                    tabBarInactiveTintColor: "gray",
-                    tabBarActiveTintColor: "black"
                 }}
             />
             <Tabs.Screen
                 name="profile"
                 options={{
                     tabBarIcon: ({ color }) => <Feather name="user" size={22} color={color} />,
-                    tabBarInactiveTintColor: "gray",
-                    tabBarActiveTintColor: "black"
                 }}
             />
         </Tabs>
